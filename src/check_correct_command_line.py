b807479286209624b380e42cec8aced7cab65aa0
ZAYY <806096961@qq.com>
2017-05-28 17:33:33 +0200

    correct

--- src/check_correct_command_line.py
+++ src/check_correct_command_line.py
@@ -1,109 +1,35 @@
 import sys, getopt
 import math
 
-def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print 'error, usage, *.py -i <inputfile> -o <outputfile>'
-      sys.exit(2)
-   for option, argument in opts:
-      if option == '-help':
-         print 'help you, *.py -i <inputfile> -o <outputfile>'
-         sys.exit()
-      elif option in ("-i", "--inputfile"):
-         inputfile = argument
-      elif option in ("-o", "--outputfile"):
-         outputfile = argument
-#   print 'Input file is "', inputfile
-#   print 'Output file is "', outputfile
 
-
-def take_standard_data(predictedclass):
-    if (predictedclass== posture1):
-#        standard_data = posture1_standard_data # loaded data
-
-    if (predictedclass== posture2):
-        standard_data = posture2_standard_data
-
-    if (predictedclass== posture3):
-        standard_data = posture3_standard_data
-
-    if (predictedclass== posture4):
-        standard_data = posture4_standard_data
-
-    if (predictedclass== posture5):
-        standard_data = posture5_standard_data
-
-    if (predictedclass== posture6):
-        standard_data = posture6_standard_data
-    print 'standard_data:', standard_data
-
-
-def distance(testingdata,standard_data):
+def correct(testingdata,standard_data,distance):
+    err = []
     
-#    fruits = ['banana', 'apple',  'mango']
-    for i in range(len(standard_data)):
-        sum_distance + =(standard_data[i]-testingdata[i])*(standard_data[i]-testingdata[i])
-
-#       error_square_sum + =(standard_data[i]-testingdata[i])*(standard_data[i]-testingdata[i])
-#    error = math.sqrt (x)(error_square_sum)
-    distance = math.sqrt (x)(sum_distance)
-#    print 'distance :', distance
-#    print 'error :', error
-
-def correct(testingdata,standard_data):
-    err1 = math.abs(standard_data[i]-testingdata[i])/distance(testingdata,standard_data)
-   max_err = math.max(err1)
-    for i in range(11):
-        if (max_err == err1[i]):
+    for i in range(len(testingdata)):
+        err1 = abs(standard_data[i]-testingdata[i])/distance
+        err.append(err1)
+    max_err = max(err)
+    for i in range(len(testingdata)):
+        if (max_err == err[i]):
             print 'the #',i, 'err,  need to modify'
 
 
-#print "Good bye!"
-
-
-def check(testingdata, predictedclass):
-        standard_data = take_standard_data(predictedclass)
-        distance = distance(testingdata,standard_data)
-        if (distance <= error_boundory):
-            print 'right'
-            break
-        else :
-            correct(testingdata,standard_data)
-            print 'the #',i, 'err, succeed modifying'
-            raw_input("Press enter to continue to learn the modified posture  ... \n")
-#            testingdata = kinect_functione_get_data() ##### call the extracted data from kinect
-
-#            predictedclass = SVM(testingdata) #####
-             check(testingdata, predictedclass)
-
+def check_if_is_right(testingdata,standardData):
+    sum_distance = 0
+    for i in range(len(standardData)):
+        sum_distance +=(standardData[i]-testingdata[i])**2
+        distance = math.sqrt(sum_distance)
+    if (distance <= 0):
+        print 'right'
+    else :
+        correct(testingdata,standardData,distance)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+
     print"Welcome IIS project, we are group 5"
-    raw_input("Press enter to train SVM... \n")
-    standard_data = inputfile
-    SVM = train_SVM(standard_data)#call the train funtion
-    raw_input("Press enter to train SVM... \n")
-
-    raw_input("Press enter to continue... \n")
-    
-    count = 1
-    while (count <= 6):
-        print 'The count is:', count
-#        testingdata = kinect_functione_get_data()
-#        predictedclass = SVM(testingdata)
-        check(testingdata, predictedclass)
-        print "succeed in this porture "
-        count = count + 1
-        if (count<=6):
-            raw_input("Press enter to continue to learn next posture  ... \n")
-        else:
-            raw_input("Congratulations, Press enter to stop  ... \n")
-
+    testingdata = [1,6]
+    standardData = [2,3]
+    check_if_is_right(testingdata, standardData)
 
 
